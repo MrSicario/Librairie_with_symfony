fe8d83aed9dcd86f0ae03cd14c429d6345a5b84d
$icario <mahahesjr@gmail.com>
2025-10-06 17:27:36 +0200

    config for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
+
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
